0ad2c18a3758d4658d239502657395e05bf58cc9
Ilya <sannikov444@yandex.ru>
2023-01-07 15:24:38 +0300

    homework

---
+++ kr/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.Clear();
+Console.Write("Введите количество элементов в массиве ");
+int n =int.Parse(Console.ReadLine());
+string []massiv = new string[n];
+string []newmassiv = new string[n];
+int j=0;
+for (int i=0;i<n;i++){
+    Console.Write($"{i+1})");
+    massiv[i]=Convert.ToString(Console.ReadLine());
+    if (massiv[i].Length<=3){
+        newmassiv[j]=massiv[i];
+        j++;
+    }
+}
+for (int i=0;i<j;i++) Console.Write($"{newmassiv[i]}    ");
